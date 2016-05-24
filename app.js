58edecb6725b0f50b53c1dc693ad48cdb0488bdc
Cadale Thomas <atlas@Cadales-MacBook.local>
2016-05-23 20:49:50 -0400

    add heroku env variable for port

--- app.js
+++ app.js
@@ -54,6 +54,6 @@ app.post('/contact/send', function(req, res){
   });
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 80);
 
-console.log('Server is running on port 3000...');
+console.log('Server is running on port ' + (process.env.PORT || 80) + '...');
